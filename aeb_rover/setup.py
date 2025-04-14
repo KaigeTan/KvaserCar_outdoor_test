70b6c30442d84e8922dba196ebd6ff891219cc6e
KaigeTan <kaiget@kth.se>
2025-04-14 22:05:39 +0200

    add aeb rover, from prior jetson code

---
+++ aeb_rover/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'aeb_rover'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='nvidia',
+    maintainer_email='kaiget@kth.se',
+    description='Autonomous Emergency Braking with Ultrasonic Sensors',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'aeb_node_open = aeb_rover.aeb_rover_node:main',  # open loop joystick control with AEB 
+            'aeb_node_closed = aeb_rover.PIctrl:main',  # closed loop control by PI with AEB
+        ],
+    },
+)
